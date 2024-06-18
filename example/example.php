a519823ffc1bbad4573b8755593ca53160a5c1a2
Louis Ouellet <louis_ouellet@hotmail.com>
2024-06-18 09:19:02 -0400

    Added Example file

---
+++ example/example.php
@@ -0,0 +1,23 @@
+<?php
+
+// Import additionnal class into the global namespace
+use LaswitchTech\coreCSRF\CSRF;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Initiate CSRF
+$CSRF = new CSRF();
+
+// Configure field
+$CSRF->config('field', 'csrf_token');
+
+// Retrieve CSRF Token
+$token = $CSRF->token();
+
+// Verify CSRF Token
+if($CSRF->verify($token)){
+    echo 'CSRF Token is valid';
+} else {
+    echo 'CSRF Token is invalid';
+}
